7f0a9fe116cd0a0150ba19fc9f0416500ce58670
cyz <chenyongzhi@huli.com>
2018-11-14 12:01:22 +0800

    add love

---
+++ love.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+
+while True:
+
+   x = raw_input("do you love me? (yes or no):")
+
+   if x==("yes"):
+     print("baby,i love you too")
+     break
+   elif x==("no"):
+     print("come, i know you love me")
+   else:
+     print("what did you say?")
+
+
+
